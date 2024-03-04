5259c769e77675dcd91b65aa2842ebd7550cd545
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-03-04 13:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const button = document.querySelector(".btn");
+const input = document.querySelector(".input");
+button.addEventListener("click",()=>{
+	input.focus();
+})
