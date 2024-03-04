c3963d0f1855a0a0eb8ed9d76495ca266f0471b8
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-03-04 13:46:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
+const search = document.querySelector(".search");
 const button = document.querySelector(".btn");
 const input = document.querySelector(".input");
 button.addEventListener("click",()=>{
 	input.focus();
+	search.style.border="1px solid red";
 })
